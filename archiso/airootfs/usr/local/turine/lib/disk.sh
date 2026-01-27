44876cf16baa79bea9eef4893ca5c2f9ad0abdfe
Rimkomatic <mahapatrarik123@gmail.com>
2026-01-27 22:47:56 +0530

    added hello.service

--- archiso/airootfs/usr/local/turine/lib/disk.sh
+++
@@ -1,8 +0,0 @@
-wipefs -a /dev/sda
-parted /dev/sda --script mklabel gpt
-
-parted /dev/sda --script mkpart ESP fat32 1MiB 513MiB
-parted /dev/sda --script set 1 esp on
-
-parted /dev/sda --script mkpart primary linux-swap 513MiB 8.5GiB
-parted /dev/sda --script mkpart primary ext4 8.5GiB 100%
--- archiso/airootfs/usr/local/turine/lib/mount.sh
+++
@@ -1,4 +0,0 @@
-mount /dev/sda3 /mnt
-
-mkdir -p /mnt/boot
-mount /dev/sda1 /mnt/boot
