f4945d7f6a79bbbca2c38ae0081ec7da0ed19f19
rimkomatic <mahapatrarik123@gmail.com>
2026-01-29 02:53:04 +0530

    added installer.service

---
+++ archiso/airootfs/usr/local/bin/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "=================================="  
+echo "Turine installer started"           
+echo "Date: $(date)"                     
+echo "Running inside ArchISO"            
+echo "==================================" 
+echo "Installer exiting successfully."  
+sleep infinity
--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_name="turine"
+iso_label="TURINE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
