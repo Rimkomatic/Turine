acc3835b0695e69f2a3858b3bbfe1e6e84b5b5b5
rimkomatic <mahapatrarik123@gmail.com>
2026-01-29 17:46:31 +0530

    added iso builder script

--- archiso/airootfs/usr/local/bin/install.sh
+++ archiso/airootfs/usr/local/bin/install.sh
@@ -2,6 +2,7 @@
 echo "=================================="  
 echo "Turine installer started"           
 echo "Date: $(date)"                     
+lsblk -l
 echo "Running inside ArchISO"            
 echo "==================================" 
 echo "Installer exiting successfully."  
---
+++ build-iso.sh
@@ -0,0 +1,2 @@
+sudo rm -rf work out
+sudo mkarchiso -v archiso
